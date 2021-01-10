e05c71b382db1b26b9253d681ee4893e43a5e8b5
Blaine R Love <blainelove94@gmail.com>
2021-01-10 23:06:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     puts You guessed the correct number!
   elsif user_input == 'exit'
     puts 'Goodbye!'
-  else user_input != number
+  elsif 
     puts "Sorry! The computer guessed #{number}."
   end
 end
