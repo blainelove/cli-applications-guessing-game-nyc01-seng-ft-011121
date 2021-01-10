c0b2f2c1efb660a75f294d1d7a004ec773d11ae1
Blaine R Love <blainelove94@gmail.com>
2021-01-10 23:08:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 
 def run_guessing_game
-  number = rand(1..6)
+  number = rand(1..7)
   user_input = gets.chomp
   if user_input == number
     puts You guessed the correct number!
