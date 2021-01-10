f16adb086306c4856f5700df1d23ad951cbb3c98
Blaine R Love <blainelove94@gmail.com>
2021-01-10 23:16:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   number = 5
   user_input = gets.chomp
   binding.pry
-  if user_input == number
+  if user_input.to_s == number
     puts You guessed the correct number!
   elsif user_input == 'exit'
     puts 'Goodbye!'
