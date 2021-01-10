bf645bb9b16902534bff3ddf6de8ea06440b689a
Blaine R Love <blainelove94@gmail.com>
2021-01-10 23:18:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,11 +5,11 @@ def run_guessing_game
   number = 5
   numberStr = number.to_s
   user_input = gets.chomp
-  if user_input == number
+  if user_input == numberStr
     puts You guessed the correct number!
   elsif user_input == 'exit'
     puts 'Goodbye!'
   else
-    puts "Sorry! The computer guessed #{number}."
+    puts "Sorry! The computer guessed #{numberStr}."
   end
 end
